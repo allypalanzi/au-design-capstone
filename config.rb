1942027cf9a9b085ce5c9f8e4cb56d3e9bbea6c4
ally palanzi <ampalanzi@gmail.com>
2014-04-26 22:43:00 -0400

    i forget

--- config.rb
+++ config.rb
@@ -2,15 +2,15 @@ compass_config do |config|
   config.output_style = :compact
 end
 
+configure :build do
+  activate :minify_css
+  activate :minify_javascript
+  activate :relative_assets
+end
+
 activate :livereload
 set :relative_links, true
 set :css_dir, 'stylesheets'
 set :js_dir, 'javascripts'
 set :images_dir, 'images'
 
-configure :build do
-  activate :minify_css
-  activate :minify_javascript
-  activate :asset_hash
-  activate :relative_assets
-end
